300cfb6b68feb6d004d52f247a49371f0e9c67c0
龙启强 <2048462457@qq.com>
2023-03-13 09:53:04 +0800

    更新

--- index01_1/src/com/task/exception/SexException.java
+++ index01_1/src/com/task/exception/SexException.java
@@ -3,5 +3,8 @@ package com.task.exception;
 public class SexException extends Exception {
     public SexException(String msg){
         super(msg);
+        System.out.println("杀入防火，龙启强呀");
     }
+
+
 }
